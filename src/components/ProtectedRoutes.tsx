3e7ae7c801f303f4c8bb5540112cb03455ef3c29
sandygudie <goodnewssandy@gmail.com>
2024-03-18 22:14:25 +0100

    fix:google login

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 export default function ProtectedRoutes({ children }: any) {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [cookies] = useCookies(["access_token"]);
   console.log(cookies);
   useEffect(() => {
     const verifyCookie = async () => {
       if (!cookies.access_token) {
-        navigate("/login");
+        // navigate("/login");
       }
     };
     verifyCookie();
