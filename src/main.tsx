a69ffc43422f700144f3f8fd75c5dc3b55a1feb2
sandygudie <goodnewssandy@gmail.com>
2024-03-18 22:31:56 +0100

    fix:google login

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import "styles/global.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
     <AntDesign>
       <Provider store={store}>
+      <CookiesProvider defaultSetOptions={{ path: '/' }}>
         <App />
+        </CookiesProvider>
       </Provider>
       </AntDesign>
     </BrowserRouter>
