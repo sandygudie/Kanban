90802c294febf4a0684fc57667608cd7f4239c76
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2024-03-20 16:58:34 +0100

    chore:added useragent to detect mobile device

--- src/utilis/token.ts
+++ src/utilis/token.ts
@@ -1,12 +1,15 @@
 export const TOKEN_KEY = "APP_TOKEN";
 export const LOGOUT_KEY = "APP_LOGOUT";
-
+import axios from "axios";
 export function getToken() {
+  if (typeof window !== "undefined") {
   return localStorage.getItem(TOKEN_KEY);
+  }
 }
 
 export function setToken(token: string) {
   if (typeof window !== "undefined") {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
     localStorage.setItem(TOKEN_KEY, token);
   }
 }
