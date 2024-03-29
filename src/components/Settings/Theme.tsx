0b87a72b539286c978ad98b6f397362be0ca8baa
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2024-03-29 10:57:13 +0100

    Update Theme.tsx

--- src/components/Settings/Theme.tsx
+++ src/components/Settings/Theme.tsx
@@ -1,16 +1,17 @@
 import { Checkbox } from "antd";
 import { useState } from "react";
-import { changeTheme } from "utilis";
+import { TitleCase, changeTheme } from "utilis";
 
 export default function Theme() {
-  const [theme, setTheme] = useState("");
+  const currentTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(currentTheme ? currentTheme : "");
 
   return (
     <div className="py-5 px-8 bg-secondary rounded-md">
       <h2 className="font-bold text-sm md:text-base mb-6">Theme Selection</h2>
       {[
-        { id: 1, title: "Light" },
-        { id: 2, title: "Dark" },
+        { id: 1, title: "light" },
+        { id: 2, title: "dark" }
       ].map((ele) => {
         return (
           <div className="my-4" key={ele.id}>
@@ -21,7 +22,7 @@ export default function Theme() {
                 setTheme(ele.title), changeTheme(ele.title);
               }}
             >
-              <p className="font-medium !text-inherit"> {ele.title} </p>
+              <p className="font-medium !text-inherit"> {TitleCase(ele.title)}</p>
             </Checkbox>
           </div>
         );
