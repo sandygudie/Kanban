4fe6c5759ca48414b1865a7fc632d90c50924ba8
sandygudie <goodnewssandy@gmail.com>
2024-03-31 22:40:14 +0100

    feature: updated chat in task page

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -34,20 +34,23 @@ export default function Index({ taskId }: any) {
       setMessage(" ");
     }
   };
-  console.log(chats.length);
+  console.log(chats[chats.length - 1]);
   return (
     <div className="mt-24 mb-12 text-center px-10">
-      {chats.length>0? (
-        chats?.map((ele: any) => {
+      {chats.length > 0 ? (
+        chats?.map((ele: any,i:number) => {
           return (
             <div
               key={ele.id}
               className={` ${
-                ele.createdBy.id === user.id
-              } ml-auto my-6 bg-gray/20 rounded-lg w-fit px-4 py-2 font-medium`}
+                ele.createdBy?.id === user.id ? "ml-auto" : "mr-auto"
+              }  my-6 bg-gray/20 rounded-lg w-fit px-4 py-2 font-medium`}
             >
-              <p className="text-xs text-left">{ele.createdBy?.name}</p>
-              <p>{ele.message}</p>
+              {
+                ele.createdBy?.name  && (
+                <p className="text-xs text-left pb-2">{ele[i]}{ele.createdBy?.name}</p>
+              )}
+              <p className="text-sm">{ele.message}</p>
             </div>
           );
         })
